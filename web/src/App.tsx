ecb64f58fff2500a91cbf80f9513ec2fdd85153a
Michele Curtaz <mcurtaz@gmail.com>
2024-08-13 17:02:35 +0200

    removed readme.md

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
