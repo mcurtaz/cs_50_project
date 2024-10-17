def564b5a1e313053f82225d4c680438ef5d62cc
Michele Curtaz <mcurtaz@gmail.com>
2024-10-17 18:18:03 +0200

    test for series + readme

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 It's a web app to keep track of think you'd like to do. Is like a to do list but instead of things you have to do it’s for activities you'd enjoy.
 
 The backend is a REST API written using Python and flask. PostgreSQL is used as database.
+The client is written using React with Typescript and Tailwind CSS.
 
 ### Run locally
 
@@ -32,3 +33,26 @@ $ cd web
 $ npm install
 $ npm run dev
 ```
+
+### Notes
+
+#### REST API
+The REST API is written with Python and Flask.
+
+It uses some libraries like:
+- Flask-SQLAlchemy: it's an ORM (Object-Relational Mapping). It maps database tables to Python classes, so you can interact with your database using Python objects instead of writing raw SQL queries. You can define models (Python classes) that correspond to database tables, and SQLAlchemy handles the translation between Python and SQL.
+- Flask-Smorest: is a Flask extension that simplifies the development of REST APIs by integrating Marshmallow (for data serialization and validation), SQLAlchemy (for ORM/database interaction), and OpenAPI (for API documentation).
+- Flask-Jwt-Extended: it provides JWT utilities that helps you write authentication functionalities and secure your API endpoints.
+- Pytest: I have written some tests with this Python library.
+
+#### WEB CLIENT
+I used React as library for the Web Client. I used Typescript to make Javascript type safe. To help me with styling I used Tailwind and Shadcn ui which provides you with some basic components like buttons, select and so on.
+
+With autentication I choosed to store the access token in the session storage based on [JSON Web Token Cheat by OWASP](https://github.com/OWASP/CheatSheetSeries/blob/master/cheatsheets/JSON_Web_Token_for_Java_Cheat_Sheet.md#token-storage-on-client-side). The cheatsheet suggest that this method could be vulnerable to XSS issues and provide a solution which I think it might be overkill for an Enjoy List Webapp. Notherless I may be implement it in the future.
+
+### Future implementations
+I'd like to add more categories like music, videogames, shows etcetera.
+
+I'd like to add more filters to find items in list.
+
+I'd like to use some public API
---
+++ server/tests/test_series.py
@@ -0,0 +1,79 @@
+import os
+
+series_data = {
+    "image_url": "https://m.media-amazon.com/images/M/MV5BMjRmMTNiMTQtMDg1ZS00MGM1LWE1MGUtYjEzMGFjNWUzOWRkXkEyXkFqcGc@._V1_.jpg",
+    "title": "The Sopranos",
+    "description": "Best series ever.",
+    "rating": 5,
+    "status": "COMPLETED"
+}
+def test_series(test_client, session, access_token):
+    response = test_client.post(
+        "/series",
+        json=series_data,
+        headers={'Authorization': f'Bearer {access_token}'}
+    )
+
+    assert response.status_code == 201, "series created successfully"
+    assert response.json["image_url"] == series_data["image_url"]
+    assert response.json["title"] == series_data["title"]
+    assert response.json["description"] == series_data["description"]
+    assert response.json["rating"] == series_data["rating"]
+    assert response.json["status"] == series_data["status"]
+
+    series_id = response.json["id"]
+
+    response = test_client.get(
+        "/series",
+        headers={'Authorization': f'Bearer {access_token}'}
+    )
+
+    assert response.status_code == 200, "series in series list"
+    assert len(response.json) == 1
+    assert response.json[0]["image_url"] == series_data["image_url"]
+    assert response.json[0]["title"] == series_data["title"]
+    assert response.json[0]["description"] == series_data["description"]
+    assert response.json[0]["rating"] == series_data["rating"]
+    assert response.json[0]["status"] == series_data["status"]
+
+    response = test_client.get(
+        f"/series/{series_id}",
+        headers={'Authorization': f'Bearer {access_token}'}
+    )
+
+    assert response.status_code == 200, "get single series"
+    assert response.json["image_url"] == series_data["image_url"]
+    assert response.json["title"] == series_data["title"]
+    assert response.json["description"] == series_data["description"]
+    assert response.json["rating"] == series_data["rating"]
+    assert response.json["status"] == series_data["status"]
+
+    response = test_client.put(
+        f"/series/{series_id}",
+        headers={'Authorization': f'Bearer {access_token}'},
+        json={
+            "description": "Best series ever!",
+            "status": "COMPLETED"
+        }
+    )
+
+    assert response.status_code == 200, "put single attribute"
+    assert response.json["image_url"] == series_data["image_url"]
+    assert response.json["title"] == series_data["title"]
+    assert response.json["description"] == "Best series ever!"
+    assert response.json["rating"] == series_data["rating"]
+    assert response.json["status"] == "COMPLETED"
+
+    response = test_client.delete(
+        f"/series/{series_id}",
+        headers={'Authorization': f'Bearer {access_token}'}
+    )
+
+    assert response.status_code == 200, "series deleted"
+
+    response = test_client.get(
+        f"/series/{series_id}",
+        headers={'Authorization': f'Bearer {access_token}'}
+    )
+
+    assert response.status_code == 404, "series deleted not found"
